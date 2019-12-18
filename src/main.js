17f9c91462f8d5428c28b0742e6f39d7573af5e6
Aleks <osinaleksandr1989@gmail.com>
2019-12-18 14:02:22 +0500

    3.6 Генерирует моки и передает компоненты

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-// import {createSiteMenuTemplate} from './components/site-menu.js';
-// import {createFilterTemplate} from './components/filter.js';
-// import {createBoardTemplate} from './components/board.js';
-// import {createTaskTemplate} from './components/task.js';
-// import {createTaskEditTemplate} from './components/task-edit.js';
-// import {generateFilters} from './mock/filter.js';
-// import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
-
 import {createBoardTemplate} from './components/board.js';
 import {createFilterTemplate} from './components/filter.js';
 import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
@@ -15,7 +7,7 @@ import {createSiteMenuTemplate} from './components/site-menu.js';
 import {generateTasks} from './mock/task.js';
 import {generateFilters} from './mock/filter.js';
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 20;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -24,18 +16,17 @@ const render = (container, template, place = `beforeend`) => {
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 render(siteHeaderElement, createSiteMenuTemplate());
+
 const filters = generateFilters();
-render(siteMainElement, createFilterTemplate(filters), `beforeend`);
+render(siteMainElement, createFilterTemplate(filters));
 render(siteMainElement, createBoardTemplate());
 
 const taskListElement = document.querySelector(`.board__tasks`);
 render(taskListElement, createTaskEditTemplate());
+const tasks = generateTasks(TASK_COUNT);
 
-new Array(TASK_COUNT)
-  .fill(``)
-  .forEach(
-      () => render(taskListElement, createTaskTemplate())
-  );
+render(taskListElement, createTaskEditTemplate(tasks[0]));
+tasks.slice(1).forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createLoadMoreButtonTemplate());
