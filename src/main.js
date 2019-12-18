9307bd491eb74ff48935fe99f504005feccd2c8e
Aleks <osinaleksandr1989@gmail.com>
2019-12-18 13:42:09 +0500

    3.5 импортирует моки в точку входа

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import {createSiteMenuTemplate} from './components/site-menu.js';
-import {createFilterTemplate} from './components/filter.js';
+// import {createSiteMenuTemplate} from './components/site-menu.js';
+// import {createFilterTemplate} from './components/filter.js';
+// import {createBoardTemplate} from './components/board.js';
+// import {createTaskTemplate} from './components/task.js';
+// import {createTaskEditTemplate} from './components/task-edit.js';
+// import {generateFilters} from './mock/filter.js';
+// import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
+
 import {createBoardTemplate} from './components/board.js';
-import {createTaskTemplate} from './components/task.js';
-import {createTaskEditTemplate} from './components/task-edit.js';
-import {generateFilters} from './mock/filter.js';
+import {createFilterTemplate} from './components/filter.js';
 import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
+import {createTaskEditTemplate} from './components/task-edit.js';
+import {createTaskTemplate} from './components/task.js';
+import {createSiteMenuTemplate} from './components/site-menu.js';
+import {generateTasks} from './mock/task.js';
+import {generateFilters} from './mock/filter.js';
 
 const TASK_COUNT = 3;
 
