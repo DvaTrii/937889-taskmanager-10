87b63fe53305e3502aa9a3dd84c2315b9adf54c7
Aleks <osinaleksandr1989@gmail.com>
2020-01-04 18:46:59 +0500

    5.1.17 Добавит метод установки обработчика submit формы редактирования задания

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -182,4 +182,9 @@ export default class TaskEdit extends AbstractComponent {
   getTemplate() {
     return createTaskEditTemplate(this._task);
   }
+
+  setSubmitHandler(handler) {
+    this.getElement().querySelector(`form`)
+      .addEventListener(`submit`, handler);
+  }
 }
