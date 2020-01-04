9d4e7dacca76890b2bb8396933cf23c8531fb580
Aleks <osinaleksandr1989@gmail.com>
2020-01-04 17:51:44 +0500

    5.1.10 Наследует компонент карточки от абстрактного класса

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,6 @@
+import AbstractComponent from "./abstract-component.js";
 import {MONTH_NAMES} from "../const";
-import {createElement, formatTime} from '../utils.js';
+import {formatTime} from '../utils.js';
 
 const createHashtagsMarkup = (hashtags) => {
   return hashtags
@@ -86,26 +87,14 @@ const createTaskTemplate = (task) => {
   );
 };
 
-export default class Task {
+export default class Task extends AbstractComponent {
   constructor(task) {
-    this._task = task;
+    super();
 
-    this._element = null;
+    this._task = task;
   }
 
   getTemplate() {
     return createTaskTemplate(this._task);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
