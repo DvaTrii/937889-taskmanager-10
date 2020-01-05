fb6620fc10877769294dc4ace6ed6818344872b9
Aleks <osinaleksandr1989@gmail.com>
2020-01-05 10:13:01 +0500

    5.2.1 Добавит виды сортировки

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,16 +1,28 @@
 import AbstractComponent from './abstract-component.js';
 
+export const SortType = {
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`,
+  DEFAULT: `default`,
+};
+
 const createSortTemplate = () => {
   return (
     `<div class="board__filter-list">
-      <a href="#" class="board__filter">SORT BY DEFAULT</a>
-      <a href="#" class="board__filter">SORT BY DATE up</a>
-      <a href="#" class="board__filter">SORT BY DATE down</a>
+      <a href="#" data-sort-type="${SortType.DEFAULT}" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" data-sort-type="${SortType.DATE_UP}" class="board__filter">SORT BY DATE up</a>
+      <a href="#" data-sort-type="${SortType.DATE_DOWN}" class="board__filter">SORT BY DATE down</a>
     </div>`
   );
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currenSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortTemplate();
   }
