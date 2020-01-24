0c8074e80f6e819984b4ca07c9dce4be1b9a6333
Aleks <osinaleksandr1989@gmail.com>
2020-01-24 19:56:43 +0500

    6.1.4 Подключит moment

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 const path = require(`path`);
 
 module.exports = {
@@ -16,5 +17,10 @@ module.exports = {
     // Если не работает по стандартному URLу в браузере ‘http:/localhost:8080’,
     // то добавьте к нему ‘/webpack-dev-server/‘: ‘http:/localhost:8080/webpack-dev-server/'
     watchContentBase: true
-  }
+  },
+  plugins: [
+    new MomentLocalesPlugin({
+      localesToKeep: [`es-us`],
+    })
+  ]
 };
