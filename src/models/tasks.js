c76260b5c2898983e14c554c2fab02a7902e3e9d
Aleks <osinaleksandr1989@gmail.com>
2020-01-27 12:15:25 +0500

    7.1.2 Добавит в модель методы setTasks getTasks

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -2,4 +2,12 @@ export default class Tasks {
   constructor() {
     this._tasks = [];
   }
+
+  getTasks() {
+    return this._tasks;
+  }
+
+  setTasks(tasks) {
+    this._tasks = Array.from(tasks);
+  }
 }
