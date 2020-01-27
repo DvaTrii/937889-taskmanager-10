572e0f985c0a09b302d70fdf2939c817e1b6251b
Aleks <osinaleksandr1989@gmail.com>
2020-01-27 12:51:32 +0500

    7.1.8 Опишет вспомогательные функции для фильтрации

--- src/const.js
+++ src/const.js
@@ -16,3 +16,13 @@ export const MONTH_NAMES = [
   `November`,
   `December`,
 ];
+
+export const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVORITES: `favorites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TAGS: `tags`,
+  TODAY: `today`,
+};
