d06dcac41c866e4d439e35d50057d1efb64ec507
Aleks <osinaleksandr1989@gmail.com>
2020-01-27 12:58:02 +0500

    7.1.9 Научит модель учиывать выбранный фильтр

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,14 +1,29 @@
+import {getTasksByFilter} from '../utils/filter.js';
+import {FilterType} from '../const.js';
+
 export default class Tasks {
   constructor() {
     this._tasks = [];
+    this._activeFilterType = FilterType.ALL;
+
+    this._filterChangeHandlers = [];
   }
 
   getTasks() {
+    return getTasksByFilter(this._tasks, this._activeFilterType);
+  }
+
+  getTasksAll() {
     return this._tasks;
   }
 
   setTasks(tasks) {
     this._tasks = Array.from(tasks);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._filterChangeHandlers.forEach((handler) => handler());
   }
 
   updateTask(id, task) {
@@ -21,5 +36,9 @@ export default class Tasks {
     this._tasks = [].concat(this._tasks.slice(0, index), task, this._tasks.slice(index + 1));
 
     return true;
+  }
+
+  setFilterChangeHandler(handler) {
+    this._filterChangeHandlers.push(handler);
   }
 }
