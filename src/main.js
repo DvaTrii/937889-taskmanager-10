8b4db5da957d7d18004b00f98ba78299832de7ff
Aleks <osinaleksandr1989@gmail.com>
2020-01-27 13:20:58 +0500

    7.1.13 Подключит фильтры в точку входа

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import BoardComponent from './components/board.js';
 import BoardController from "./controllers/board";
-import FilterComponent from './components/filter.js';
+import FilterController from './controllers/filter.js';
 import SiteMenuComponent from './components/site-menu.js';
 import TasksModel from './models/tasks.js';
 import {generateTasks} from './mock/task.js';
-import {generateFilters} from './mock/filter.js';
 import {render, RenderPosition} from "./utils/render.js";
 
 const TASK_COUNT = 20;
@@ -13,16 +12,15 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
-
-const filters = generateFilters();
-render(siteMainElement, new FilterComponent(filters), RenderPosition.BEFOREEND);
-
-const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
-
 const tasks = generateTasks(TASK_COUNT);
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
+
+const filterController = new FilterController(siteMainElement, tasksModel);
+filterController.render();
+
+const boardComponent = new BoardComponent();
+render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 
 const boardController = new BoardController(boardComponent, tasksModel);
 
