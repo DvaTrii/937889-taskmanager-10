79a1ba24fc532143f4b67775d10c563f68e252e1
Aleks <osinaleksandr1989@gmail.com>
2020-07-05 15:48:08 +0500

    == Исправит main - создаст полноценный компонент меню ==

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import BoardComponent from './components/board.js';
 import BoardController from "./controllers/board";
 import FilterController from './controllers/filter.js';
-import SiteMenuComponent from './components/site-menu.js';
+import SiteMenuComponent, {MenuItem} from './components/site-menu.js';
 import TasksModel from './models/tasks.js';
 import {generateTasks} from './mock/task.js';
 import {render, RenderPosition} from "./utils/render.js";
@@ -12,15 +12,16 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 const siteMenuComponent = new SiteMenuComponent();
 
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+
+
 // Быстрое решение для подписки на клик по кнопке.
 // Это противоречит нашей архитектуре работы с DOM-элементами, но это временное решение.
 // Совсем скоро мы создадим полноценный компонент для работы с меню.
-siteMenuComponent.getElement().querySelector(`.control__label--new-task`)
-  .addEventListener(`click`, () => {
-    boardController.createTask();
-  });
-
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+// siteMenuComponent.getElement().querySelector(`.control__label--new-task`)
+//   .addEventListener(`click`, () => {
+//     boardController.createTask();
+//   });
 
 const tasks = generateTasks(TASK_COUNT);
 const tasksModel = new TasksModel();
@@ -33,5 +34,13 @@ const boardComponent = new BoardComponent();
 render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 
 const boardController = new BoardController(boardComponent, tasksModel);
-
 boardController.render();
+
+siteMenuComponent.setOnChange((menuItem) => {
+  switch (menuItem) {
+    case MenuItem.NEW_TASK:
+      siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      boardController.createTask();
+      break;
+  }
+});
