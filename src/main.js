79504540fb7399ff9ac7b6dd1a93bf85ddd2e0df
Aleks <osinaleksandr1989@gmail.com>
2020-07-09 14:53:52 +0500

    8.1.1 Рефакторит main.js

--- src/main.js
+++ src/main.js
@@ -9,35 +9,6 @@ import {render, RenderPosition} from "./utils/render.js";
 
 const TASK_COUNT = 20;
 
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-const siteMenuComponent = new SiteMenuComponent();
-
-render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
-
-
-// Быстрое решение для подписки на клик по кнопке.
-// Это противоречит нашей архитектуре работы с DOM-элементами, но это временное решение.
-// Совсем скоро мы создадим полноценный компонент для работы с меню.
-// siteMenuComponent.getElement().querySelector(`.control__label--new-task`)
-//   .addEventListener(`click`, () => {
-//     boardController.createTask();
-//   });
-
-const tasks = generateTasks(TASK_COUNT);
-const tasksModel = new TasksModel();
-tasksModel.setTasks(tasks);
-
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
-
-const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
-
-const boardController = new BoardController(boardComponent, tasksModel);
-boardController.render();
-
-
 const dateTo = new Date();
 const dateFrom = (() => {
   const d = new Date(dateTo);
@@ -45,10 +16,25 @@ const dateFrom = (() => {
   return d;
 })();
 
-const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
-render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
-statisticsComponent.hide();
+const tasks = generateTasks(TASK_COUNT);
+const tasksModel = new TasksModel();
 
+tasksModel.setTasks(tasks);
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuComponent();
+const statisticsComponent = new StatisticsComponent({tasks: tasksModel, dateFrom, dateTo});
+
+const boardComponent = new BoardComponent();
+const boardController = new BoardController(boardComponent, tasksModel);
+const filterController = new FilterController(siteMainElement, tasksModel);
+
+
+render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
+filterController.render();
+render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
+boardController.render();
 render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
 statisticsComponent.hide();
 
