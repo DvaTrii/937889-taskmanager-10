40b6be172318c73f2855b79b2044e709c2e090a9
Aleks <osinaleksandr1989@gmail.com>
2020-07-13 16:17:28 +0500

    9.1.6 Добавит реализацию

--- src/api/provider.js
+++ src/api/provider.js
@@ -35,11 +35,18 @@ export default class Provider {
 
   updateTask(id, task) {
     if (isOnline()) {
-      return this._api.updateTask(id, task);
-    }
-    // TODO: Реализовать логику при отсутствии интернета
+      return this._api.updateTask(id, task)
+        .then((newTask) => {
+          this._store.setItem(newTask.id, newTask.toRAW());
 
-    return Promise.reject(`offline logic is not implemented`);
+          return newTask;
+        });
+    }
+
+    const localTask = Task.clone(Object.assign(task, {id}));
+    this._store.setItem(id, localTask.toRAW());
+
+    return Promise.resolve(localTask);
   }
 
   deleteTask(id) {
