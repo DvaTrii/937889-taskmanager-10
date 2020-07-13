b869bc6f866092154bb203ff389a403a2b244c06
Aleks <osinaleksandr1989@gmail.com>
2020-07-13 15:55:51 +0500

    9.1.4 Добавит  с интерфейсом, но без реализации

--- src/api/provider.js
+++ src/api/provider.js
@@ -1,18 +1,26 @@
+import Task from "../models/task.js";
+
 const isOnline = () => {
   return window.navigator.onLine;
 };
 
 export default class Provider {
-  constructor(api) {
+  constructor(api, store) {
     this._api = api;
+    this._store = store;
   }
 
   getTasks() {
     if (isOnline()) {
-      return this._api.getTasks();
+      return this._api.getTasks()
+        .then((tasks) => {
+          tasks.forEach((task) => this._store.setItem(task.id, task.toRAW()));
+
+          return tasks;
+        });
     }
     // TODO: Реализовать логику при отсутствии интернета
-
+    const storeTasks = Object.values(this._store.getItems());
     return Promise.reject(`offline logic is not implemented`);
   }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import API from "./api/index.js";
 import Provider from "./api/provider.js";
+import Store from "./api/store.js";
 import BoardComponent from './components/board.js';
 import StatisticsComponent from "./components/statistics.js";
 import BoardController from "./controllers/board";
@@ -19,7 +20,8 @@ const dateFrom = (() => {
 })();
 
 const api = new API(END_POINT, AUTHORIZATION);
-const apiWithProvider = new Provider(api);
+const store = new Store();
+const apiWithProvider = new Provider(api, store);
 
 const tasksModel = new TasksModel();
 
